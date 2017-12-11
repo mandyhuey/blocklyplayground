fb8f17e980a5f262f6a117ad15eca628c86a5e14
Mandy <server@104.131.145.133>
2017-12-10 23:29:27 -0800

    Added characters and say block

--- customBlocks.js
+++ customBlocks.js
@@ -31,7 +31,7 @@ Blockly.Blocks['clearscreen'] = {
 Blockly.Blocks['catpose'] = {
     init: function() {
         this.appendDummyInput()
-            .appendField("Change Pose")
+            .appendField("Change cat pose")
             .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "catIndex");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -39,6 +39,33 @@ Blockly.Blocks['catpose'] = {
         this.setTooltip("");
         this.setHelpUrl("");
     }
+};
+
+Blockly.Blocks['minionpose'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Change minion pose")
+            .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "minionIndex");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['say'] = {
+  init: function() {
+    this.appendValueInput("displayWord")
+    .setCheck("String")
+    .appendField("Say");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(330);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
 };
 
 Blockly.JavaScript['whenrunclicked'] = function(block) {
@@ -54,6 +81,21 @@ Blockly.JavaScript['clearscreen'] = function(block) {
 
 Blockly.JavaScript['catpose'] = function(block) {
     var dropdown_catindex = block.getFieldValue('catIndex');
-    var blockCode = 'updatePose('+dropdown_catindex+');';
+    var blockCode = 'updatePose('+dropdown_catindex+', 1);';
     return blockCode;
+};
+
+Blockly.JavaScript['minionpose'] = function(block) {
+    var dropdown_minionindex = block.getFieldValue('minionIndex');
+    var blockCode = 'updatePose('+dropdown_minionindex+', 2);';
+    return blockCode;
+};
+
+
+Blockly.JavaScript['say'] = function(block) {
+  var value_displayword = Blockly.JavaScript.valueToCode(block, 'displayWord', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var blockCode = 'updateSayings('+value_displayword+');';
+  console.log(value_displayword);
+  return blockCode;
 };
